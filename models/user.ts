328e82a6e042c448cb13820126c0115d62caf06a
Georg Windhaber <georg.windhaber@gmail.com>
2022-05-24 17:51:12 +0200

    Add mongo user functions

--- models/user.ts
+++ models/user.ts
@@ -1,9 +1,23 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const UserSchema = new mongoose.Schema({
   name: String,
-  email: String,
-  password: String,
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
 });
+
+UserSchema.pre("save", async function (next) {
+  const hash = await bcrypt.hash(this.password, 10);
+
+  this.password = hash;
+  next();
+});
+
+UserSchema.methods.isValidPassword = async function (password: string) {
+  const compare = await bcrypt.compare(password, this.password);
+  
+  return compare;
+};
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
