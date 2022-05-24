6145b2f045084f704ed7aa260523d16963fd673a
Georg Windhaber <georg.windhaber@gmail.com>
2022-05-24 17:39:30 +0200

    Add login session route

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,1 +1,26 @@
-import {withIronSessionRoute} from "iron-session/next"
+import { withIronSessionApiRoute } from "iron-session/next";
+import { NextApiRequest, NextApiResponse } from "next";
+import dbConnect from "../../lib/dbConnect";
+import User from "../../models/user";
+
+const sessionOptions = {
+  cookieName: "kellerbierrunde",
+  password: process.env.IRON_SESSION_PASSWORD,
+  cookieOptions: {
+    secure: process.env.NODE_ENV === "production",
+  },
+};
+
+export default withIronSessionApiRoute(loginRoute, sessionOptions);
+async function loginRoute(req: NextApiRequest, res: NextApiResponse) {
+  // get user from db
+  const { email } = await req.body;
+  await dbConnect();
+  const users = await User.find({ email });
+
+  // Check if password matches
+  if(users.length) {
+    
+  }
+  res.send({ users });
+}
--- pages/login.tsx
+++ pages/login.tsx
@@ -1,18 +1,32 @@
 import { Button, Center, Container, Input, Stack } from "@chakra-ui/react";
 import { NextPage } from "next";
+import axios from "axios";
+import { useState } from "react";
 
 const Login: NextPage = () => {
+  async function login() {
+    const user = await axios.post("/api/login", { email });
+    console.log(user.data);
+  }
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Center h={"100%"}>
       <Stack spacing={4}>
-        <Input placeholder="Email" />
+        <Input
+          placeholder="Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
         <Input placeholder="Passwort" />
-        <Button display="flex" className="primary">
+        <Button display="flex" className="primary" onClick={login}>
           Login
         </Button>
       </Stack>
     </Center>
   );
-}
+};
 
 export default Login;
